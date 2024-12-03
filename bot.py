32b514b1a6ed4905beecf05c45a6378e01c8e33a
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2024-12-03 11:16:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 import random
 import asyncio
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,72 +14,14 @@ API_TOKEN = '7863411238:AAHCH7DhGTifxoiv3mpUtK4icVLcjAC6KjE'
 CHAT_ID = '-1002033347065'  # Replace with your group chat ID
 
 # List of branches and function to get a random amount
-branches = ["AWASH", "OROMIA", "CBE","ABYSSINIA"]
+branches = ["AWASH", "OROMIA", "CBE", "ABYSSINIA"]
 
 def get_random_amount():
     return random.randint(86, 167) * 100  # Generates random amounts between 8600 and 16700, divisible by 100
 
 # User list
 user_list = [
-    "Xilaahun Gamada", "Mulu Waaqjiraa", "Firaol Megersa", "Yeshi Tesfaye", "Fayyisaa Woldemichael",
-"Jemila Beshir", "Abdi Guddinaa", "Tariku Daba", "Samiira Qabnaa", "Taddese Melaku",
-"Waaqgari Birraa", "Rahel Girma", "Matiwos Fufa", "Ibtisaa Olani", "Genet Biruk",
-"Rooba Dargii", "Mulu Bayissa", "Netsanet Demissie", "Gadaa Sabaa", "Asaffaa Jalataa",
-"Meskerem Bekele", "Tafarraa Guddisaa", "Hibboo Biraanuu", "Solomon Alemayehu",
-"Xinnaan Lammii", "Zahra Ali", "Waaqtolee Bulaa", "Getachew Zeleke", "Firaasaa Dirribaa",
-
-"Tsehay Shiferaw", "Saron Fikre", "Yared Hailemariam","Morkataa Daggafaa", "Sanyii Tafarraa", "Rooba Daadhii", 
-"Hana Fikadu", "Meron Birhanu", "Gammachiis Roobaa", "Sabboonaa Hordofaa", "Galanii Lammii", 
-"Aaddee Bontuu", "Baayisaa Hirphasaa", "Dureessaa Iyyaasaa", 
-"Dawit Yohannes", "Alemayehu Kebede", "Rahel Tsegaye", "Arfasa Gammachuu", "Fira'ol Dabalaa", "Irreechaa Abdiisaa", 
-"Bereket Haile", "Selamawit Mulugeta", "Mikiyas Worku", "Galataa Haroon", "Solanaa Guddataa", "Safuu Bultii", 
-"Tigist Alemu", "Yohannes Dagnachew", "Tewodros Adane", "Gannatii Dachaasaa", "Ifaa Tolasa", "Dureettii Guyyaa", 
-"Fileembar Waltajjii", "Singitan Kebede", "Keebeek Sabbooqaa", "Jaallannee Mohammed", "Keebor Dastaa",
-    "Kookeet Amanuel", "Kee’ol Gurmeessaa", "Hawwii Galaanaa", "Keeraaj Amantee",
-    "Beekamaa Birraa", "Naafsiin Jaallataa", "Netsanet Gebremadin", "Tsagae Mokonin", "Kadija Naasir", 
-    "Marsiimoy Caalaa", "Beekaa Garramuu", "Mahilet Birhanu", "Naanaat Jabeessaa", "Dinqaa Boruu", "Iftuu Bekele",
-    "Biftu Jirata", "Sahil Abdul", "Firomsa Gebeyo", "Obsineet Molaatuu", "Faayaa Toleeraa", "Siinboonee Namarraa",
-"Eyob Mulugeta", "Amensisa Wondwosen", "Sintayehu Wolde", "Shewaye Desta", "Lemlem Wondimu", "Wossen Tesfaye", 
-"Bilisee Tolaa", "Kebede Boru", "Lalise Gabbisaa", "Dachaasaa Jaleta", "Kumsa Ayala", "Siifan Sori", "Lammii Didaa", 
-"Abdiisaa Ibsa", "Dabalaa Badhaasaa", "Saba Kumsaa", "Qana'aa Gobana", "Galataa Dirribi", "Leenco Fido", 
-"Mesfin Kassa", "Sintayehu Tekle", "Betelhem Bekele", "Tamrat Hailu", "Kassa Mengistu", "Mulu Birru", 
-"Caalaa Teshome", "Marga Keno", "Galana Elemo", "Obsa Dinsa", "Ayantu Gudeta", "Hunde Gammadaa", 
-"Yalem Taye", "Gizachew Feleke", "Habte Giorgis", "Tsion Mekuria", "Getahun Tsegaye", "Blen Kebede", "Zelalem Wolde",
-"Abdisa Tufa", "Lensa Galata", "Jemal Fufa", "Bontu Geda", "Fayyisa Abebe", "Girmaa Morka", 
-
-
-"Yusuf Teshome", "Nardos Kedir", "Chaltu Guddinaa", "Amir Kedir", "Gadissa Gudina",
-"Aynalem Zewde", "Fayyisaa Basha", "Zamzam Tadesse", "Fizal Hordofaa", "Soreti Mekonnen",
-"Abdulkerim Muktar", "Rahel Ameer", "Jemal Abarraa", "Makda Abebe", "Asayehn Waqgari",
-"Sado Sora", "Biruk Qananii", "Hibbo Saanii", "Kuma Marda", "Dawit Daud",
-"Mariyam Abdurrahman", "Dima Abdi", "Fawziya Fufaa", "Badhaasoo Kennaa", "Tiruneh Meles",
-"Lemmi Tadesse", "Abdulaziz Beshir", "Ibtisam Hailu", "Mekdes Demissie", "Aida Gammadoo",
- "Yadessa Lammii", "Soriya Hordofaa", "Asaffaa Jaalataa", "Bilisummaa Baalu", "Mulu Mardaa",
-"Nasir Kedir", "Tafara Bultum", "Samiira Daba", "Ameer Gammachuu", "Haji Abdi",
-"Xilaahun Daba", "Farhia Shifa", "Ruqayya Beshir", "Getachew Lata", "Tigist Lammii",
-"Jemila Abdurrahman", "Zawadi Teshome", "Fayyidaa Gurracha", "Meron Yadessa", "Diriba Gudina",
-"Haile Asefa", "Fatima Basha", "Adama Waqjira", "Aster Daba", "Girma Waaqjira",
-"Abiy Bultum", "Tsedale Kedir", "Amani Demissie", "Zehar Guddinaa", "Sora Abdi",
-"Mulugeta Abate", "Solomon Yilma", "Tigist Habtemariam", "Amanuu Tolosaa", "Duguma Gobanaa",  
-"Kaayyoo Lataa", "Dabalaa Roobaa", "Mulugeetaa Dheeressaa", "Solan Hordofaa", "Tigabu Girma", "Sahle Selassie", "Melat Yohannes", 
-"Tolesaa Dabalaa", "Qananii Dheeressaa", "Obsa Waaqjiraa", "Walabummaa Tufa", "Caaltuu Daggafaa", "Alamayyoo Rorroo", 
-"Sisay Girma", "Hirut Tadesse", "Beza Mengistu", "Tigabu Melaku", "Mariam Abate", "Kebede Teshome", 
-"Nashida Qabena", "Sabontuu Magarsaa", "Xiyya Sabboonaa", 
-
- "Hunde Dinkisa", "Bedasa Abeya", "Abdisa Emiru", "Bilise Solomon", "Lali Habtamu", "Geda Jirenyaa", "Biniyam Mokonnin", "Michuu Daniel", 
-    "Darartu Hambisa", "Biftu Olani", "Milkii Debala", "Musa Ebrahim", "Yosef Desale", "Samuel Tsana", "Bontu Husen", "Waggaarii Birruu", 
-    "Ayantu Ijigu", "Belina Wakjira", "Yodit Tadese", "Bikiltu Oli", "Mubarek Sultan", "Ketoran Asefa", "Roba Dereje", 
-    "Kulani Teshome", "Chali Debela", "Sena Bedane", "Gutama Idosa", "Lalisa Ayala", "Kenbon Taye",
-    "Monet Belay","Hawi Taresa","Dachaa Ga'eera","Fata Yihun","Toofiq Likisa","Gamme Fayera","Tamirat Utura","Ferhan Ahmed",
-    "Yeroosan Masqaloo", "Yordanos Beki", "Hana Markos", "Rabbirraa Wasihuun",
-"Waaqjiraa Baqala", "Abarraa Bunaa", "Nagaa Eebbisaa", "Injifannoo Tasfahuun", "Malkamuu Olana",
-"Fekadu Alemu", "Mahlet Ayele", "Adanech Worku", "Yonas Eshetu", "Mekonnen Teshale", "Getaneh Daba", 
-"Dagmawit Terefe", "Abera Bekele", "Semira Mohammed", "Nigist Fikadu", "Tadele Haile", "Abayneh Tilahun",
-"Darajjee Tolasaa", "Hirphasaa Fira'ol", "Hawwi Yadataa", "Alaaz Garoma", "Tolosaa Hunde", 
-"Jirenyaa Zallaqaa", "Guddinaa Bultii", "Sirbaa Dibaabaa", "Odaa Dinquu", "Barisee Jatanii", "Guyyaa Guutamaa",  
- "Obsinet Chala", "Kennaa Jireenyaa", "Amantii Kabbadee", "Sabboontuu Olaanii", "Caaltuu Birrannaa",
-     
-"Lulit Wondimu", "Addis Mihretu", "Meaza Tesfaye", "Fasil Demeke", "Yared Amare", "Alem Tadesse",
+  "Lulit Wondimu", "Addis Mihretu", "Meaza Tesfaye", "Fasil Demeke", "Yared Amare", "Alem Tadesse",
     "Ibrahim Mohammed", "Ashu Bilishu", "Sena Adame", "Sisay Eshetu", "Yohanis Ebisa", "Gadise Waktole", "Natinael Bedasa", "Meti Melaku", 
     "Waadaa Ebisa", "Lalise Dhugasa", "Muhee Abduu", "Lammii Warquu", "Yadesa Regasa", "Fanose Dagi", "Sidise Ababa", "Bonsa Alamu", 
     "Carraa Bultii", "Shire Abdela", "Sudesi Shamil", "Abdi Matewos", "Chala Fufaa", "Amaddin Teha", "Thomas Wako",
@@ -134,6 +77,19 @@ user_list = [
 "Haimanot Abebe", "Getu Bekele", "Mebratu Tadesse","Barriisaa Magarsaa", "Jirenyaa Tolasaa", 
 "Liyuwork Assefa", "Tadesse Mekuria", "Aster Tsegaye","Dureettii Badhataa", "Shifarraa Lammii", "Tulluu Godaanaa", 
 
+"Xilaahun Gamada", "Mulu Waaqjiraa", "Firaol Megersa", "Yeshi Tesfaye", "Fayyisaa Woldemichael",
+"Jemila Beshir", "Abdi Guddinaa", "Tariku Daba", "Samiira Qabnaa", "Taddese Melaku",
+"Waaqgari Birraa", "Rahel Girma", "Matiwos Fufa", "Ibtisaa Olani", "Genet Biruk",
+"Rooba Dargii", "Mulu Bayissa", "Netsanet Demissie", "Gadaa Sabaa", "Asaffaa Jalataa",
+"Meskerem Bekele", "Tafarraa Guddisaa", "Hibboo Biraanuu", "Solomon Alemayehu",
+"Xinnaan Lammii", "Zahra Ali", "Waaqtolee Bulaa", "Getachew Zeleke", "Firaasaa Dirribaa",
+
+"Tsehay Shiferaw", "Saron Fikre", "Yared Hailemariam","Morkataa Daggafaa", "Sanyii Tafarraa", "Rooba Daadhii", 
+"Hana Fikadu", "Meron Birhanu", "Gammachiis Roobaa", "Sabboonaa Hordofaa", "Galanii Lammii", 
+"Aaddee Bontuu", "Baayisaa Hirphasaa", "Dureessaa Iyyaasaa", 
+"Dawit Yohannes", "Alemayehu Kebede", "Rahel Tsegaye", "Arfasa Gammachuu", "Fira'ol Dabalaa", "Irreechaa Abdiisaa", 
+"Bereket Haile", "Selamawit Mulugeta", "Mikiyas Worku", "Galataa Haroon", "Solanaa Guddataa", "Safuu Bultii", 
+"Tigist Alemu", "Yohannes Dagnachew", "Tewodros Adane", "Gannatii Dachaasaa", "Ifaa Tolasa", "Dureettii Guyyaa", 
 
 "Amira Waaqjira", "Seyfu Jirataa", "Nardos Abebe", "Bashir Firaol", "Ayantu Hordofaa",
 "Tadesse Abdirahman", "Hana Muna", "Jemal Fufaa", "Sorbona Gammachuu", "Dawit Alama",
@@ -145,12 +101,51 @@ user_list = [
 "Bezawit Bekele", "Abenezer Gashaw", "Rediet Getachew", "Fikadu Girma", "Mulu Abebe", "Yeshiwork Ayele", 
 "Kumaa Girmaa", "Bashannanaa Lataa", "Hibboo Galgaloo", "Haimanot Abebe", "Getu Bekele", "Mebratu Tadesse", 
 "Henok Berhanu", "Mekdes Tesfaye", "Habtamu Tefera", "Fitsum Workneh", "Hailu Desalegn", "Amanuel Shiferaw",
+   "Fileembar Waltajjii", "Singitan Kebede", "Keebeek Sabbooqaa", "Jaallannee Mohammed", "Keebor Dastaa",
+    "Kookeet Amanuel", "Kee’ol Gurmeessaa", "Hawwii Galaanaa", "Keeraaj Amantee",
+    "Beekamaa Birraa", "Naafsiin Jaallataa", "Netsanet Gebremadin", "Tsagae Mokonin", "Kadija Naasir", 
+    "Marsiimoy Caalaa", "Beekaa Garramuu", "Mahilet Birhanu", "Naanaat Jabeessaa", "Dinqaa Boruu", "Iftuu Bekele",
+    "Biftu Jirata", "Sahil Abdul", "Firomsa Gebeyo", "Obsineet Molaatuu", "Faayaa Toleeraa", "Siinboonee Namarraa",
+"Eyob Mulugeta", "Amensisa Wondwosen", "Sintayehu Wolde", "Shewaye Desta", "Lemlem Wondimu", "Wossen Tesfaye", 
+"Bilisee Tolaa", "Kebede Boru", "Lalise Gabbisaa", "Dachaasaa Jaleta", "Kumsa Ayala", "Siifan Sori", "Lammii Didaa", 
+"Abdiisaa Ibsa", "Dabalaa Badhaasaa", "Saba Kumsaa", "Qana'aa Gobana", "Galataa Dirribi", "Leenco Fido", 
+"Mesfin Kassa", "Sintayehu Tekle", "Betelhem Bekele", "Tamrat Hailu", "Kassa Mengistu", "Mulu Birru", 
+"Caalaa Teshome", "Marga Keno", "Galana Elemo", "Obsa Dinsa", "Ayantu Gudeta", "Hunde Gammadaa", 
+"Yalem Taye", "Gizachew Feleke", "Habte Giorgis", "Tsion Mekuria", "Getahun Tsegaye", "Blen Kebede", "Zelalem Wolde",
+"Abdisa Tufa", "Lensa Galata", "Jemal Fufa", "Bontu Geda", "Fayyisa Abebe", "Girmaa Morka", 
 
 
+"Yusuf Teshome", "Nardos Kedir", "Chaltu Guddinaa", "Amir Kedir", "Gadissa Gudina",
+"Aynalem Zewde", "Fayyisaa Basha", "Zamzam Tadesse", "Fizal Hordofaa", "Soreti Mekonnen",
+"Abdulkerim Muktar", "Rahel Ameer", "Jemal Abarraa", "Makda Abebe", "Asayehn Waqgari",
+"Sado Sora", "Biruk Qananii", "Hibbo Saanii", "Kuma Marda", "Dawit Daud",
+"Mariyam Abdurrahman", "Dima Abdi", "Fawziya Fufaa", "Badhaasoo Kennaa", "Tiruneh Meles",
+"Lemmi Tadesse", "Abdulaziz Beshir", "Ibtisam Hailu", "Mekdes Demissie", "Aida Gammadoo",
+ "Yadessa Lammii", "Soriya Hordofaa", "Asaffaa Jaalataa", "Bilisummaa Baalu", "Mulu Mardaa",
+"Nasir Kedir", "Tafara Bultum", "Samiira Daba", "Ameer Gammachuu", "Haji Abdi",
+"Xilaahun Daba", "Farhia Shifa", "Ruqayya Beshir", "Getachew Lata", "Tigist Lammii",
+"Jemila Abdurrahman", "Zawadi Teshome", "Fayyidaa Gurracha", "Meron Yadessa", "Diriba Gudina",
+"Haile Asefa", "Fatima Basha", "Adama Waqjira", "Aster Daba", "Girma Waaqjira",
+"Abiy Bultum", "Tsedale Kedir", "Amani Demissie", "Zehar Guddinaa", "Sora Abdi",
+"Mulugeta Abate", "Solomon Yilma", "Tigist Habtemariam", "Amanuu Tolosaa", "Duguma Gobanaa",  
+"Kaayyoo Lataa", "Dabalaa Roobaa", "Mulugeetaa Dheeressaa", "Solan Hordofaa", "Tigabu Girma", "Sahle Selassie", "Melat Yohannes", 
+"Tolesaa Dabalaa", "Qananii Dheeressaa", "Obsa Waaqjiraa", "Walabummaa Tufa", "Caaltuu Daggafaa", "Alamayyoo Rorroo", 
+"Sisay Girma", "Hirut Tadesse", "Beza Mengistu", "Tigabu Melaku", "Mariam Abate", "Kebede Teshome", 
+"Nashida Qabena", "Sabontuu Magarsaa", "Xiyya Sabboonaa", 
 
-  
-    "Naafiroom Wakjira", "Jennenus Olleerraa", "Alsan Eebbaa", "Fayine Garoma", "Biree Gurmuu", "Jabeessaa Dhuguma",
-    "Abdii Jiraa", "Olirraa Nugusee"
+ "Hunde Dinkisa", "Bedasa Abeya", "Abdisa Emiru", "Bilise Solomon", "Lali Habtamu", "Geda Jirenyaa", "Biniyam Mokonnin", "Michuu Daniel", 
+    "Darartu Hambisa", "Biftu Olani", "Milkii Debala", "Musa Ebrahim", "Yosef Desale", "Samuel Tsana", "Bontu Husen", "Waggaarii Birruu", 
+    "Ayantu Ijigu", "Belina Wakjira", "Yodit Tadese", "Bikiltu Oli", "Mubarek Sultan", "Ketoran Asefa", "Roba Dereje", 
+    "Kulani Teshome", "Chali Debela", "Sena Bedane", "Gutama Idosa", "Lalisa Ayala", "Kenbon Taye",
+    "Monet Belay","Hawi Taresa","Dachaa Ga'eera","Fata Yihun","Toofiq Likisa","Gamme Fayera","Tamirat Utura","Ferhan Ahmed",
+    "Yeroosan Masqaloo", "Yordanos Beki", "Hana Markos", "Rabbirraa Wasihuun",
+"Waaqjiraa Baqala", "Abarraa Bunaa", "Nagaa Eebbisaa", "Injifannoo Tasfahuun", "Malkamuu Olana",
+"Fekadu Alemu", "Mahlet Ayele", "Adanech Worku", "Yonas Eshetu", "Mekonnen Teshale", "Getaneh Daba", 
+"Dagmawit Terefe", "Abera Bekele", "Semira Mohammed", "Nigist Fikadu", "Tadele Haile", "Abayneh Tilahun",
+"Darajjee Tolasaa", "Hirphasaa Fira'ol", "Hawwi Yadataa", "Alaaz Garoma", "Tolosaa Hunde", 
+"Jirenyaa Zallaqaa", "Guddinaa Bultii", "Sirbaa Dibaabaa", "Odaa Dinquu", "Barisee Jatanii", "Guyyaa Guutamaa",  
+ "Obsinet Chala", "Kennaa Jireenyaa", "Amantii Kabbadee", "Sabboontuu Olaanii", "Caaltuu Birrannaa",
+     
 ]
 
 # Function to create a transaction message for a user
@@ -161,7 +156,7 @@ def create_transaction_message(user):
 
 # Prepare transaction histories with inline button
 def create_transaction_histories():
-    histories = []  # Fixed indentation
+    histories = []
     for user in user_list:
         transaction = create_transaction_message(user)
         start_button = InlineKeyboardButton(text="Start", url="https://t.me/GudinaTumsaAirport_bot?start=ar3430555268")
@@ -178,15 +173,28 @@ async def send_transaction_histories():
     transaction_histories = create_transaction_histories()
     while True:
         for transaction, markup in transaction_histories:
-            await bot.send_message(chat_id=CHAT_ID, text=transaction, reply_markup=markup)  # Use await here
+            await bot.send_message(chat_id=CHAT_ID, text=transaction, reply_markup=markup)
             logging.info(f'Sent transaction: {transaction}')
             await asyncio.sleep(37)  # Wait for 37 seconds between each transaction message
 
-# Main function
+# Command handler to start the bot
+def start(update, context):
+    update.message.reply_text("Bot started, sending transactions...")
+
+# Main function to run the bot
 def main():
-    logging.info("Starting the bot to send transaction histories...")
-    asyncio.run(send_transaction_histories())  # Use asyncio.run to execute the async function
+    # Set up the Updater and Dispatcher
+    updater = Updater(API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Add start command handler
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Start the bot
+    updater.start_polling()
+
+    # Run the asynchronous transaction sending task
+    asyncio.run(send_transaction_histories())
 
 if __name__ == '__main__':
     main()
-
